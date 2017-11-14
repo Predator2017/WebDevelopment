61f850ab82a13ca4a77653f37d2e035cf5e64c8d
Predator2017 <31994926+Predator2017@users.noreply.github.com>
2017-11-13 23:31:43 -0600

    finished product

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,11 +1,24 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   alert("clicked on a span");
   $(this).parent().fadeOut(2000,function(){
     $(this).remove();
   });
   event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    todoText = $(this).val();
+    //create a new li and add to ul
+    $("ul").append('<li><span><i class="fa fa-trash"></i></span>'+todoText+"</li>");
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+})
